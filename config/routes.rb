2018bea20a74cc9feee1d215cfb12acbd348f934
Vikas Shah <vikas.shah@vensi.com>
2014-06-27 20:11:42 -0500

    Reverted complete support changes in index.html.erb. Style completed tasks finished.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ TasklistChallenge::Application.routes.draw do
   resources :tasks, exclude: [:show]
   root 'tasks#index'
 
-### Add completion support
-  put 'tasks/:id/archive' => 'tasks#complete', as: 'complete_task'
 ### Add archive support
-  put 'tasks/:id/archive' => 'tasks#archive', as: 'archive_task'
+  put 'tasks/:id/archive' => 'tasks#archive', as: 'archive'
   get 'readme' => 'docs#readme', as: :readme
   get 'maps' => 'maps#show', as: :maps
 end
